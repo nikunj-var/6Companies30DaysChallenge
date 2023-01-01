941a43d7f57ec4056ebf90f82b763c763cddf909
nikunj-var <nikunjvar2002@gmail.com>
2023-01-01 18:58:56 +0530

    files added

---
+++ microsoft/longesthappyprefix.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    string longestPrefix(string s)
+    {
+        int n = s.length();
+        if (n == 0)
+            return "";
+
+        int i = 0;
+        int j = 1;
+        vector<int> prefix(n, 0);
+
+        while (j < n)
+        {
+
+            if (s[i] == s[j])
+            {
+                prefix[j] = i + 1;
+                i++;
+                j++;
+            }
+            else
+            {
+                if (i == 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    i = prefix[i - 1];
+                }
+            }
+        }
+        return s.substr(0, i);
+    }
+};
